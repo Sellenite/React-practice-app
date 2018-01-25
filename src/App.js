46feda5c0789329e3f71e0c2b6b7977a42daba90
Sellenite <714627528@qq.com>
2018-01-25 10:48:37 +0800

    init

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import logo from './images/logo.svg';
 import './css/App.css';
 
+import Stage1 from './components/stage1.js';
+
 class App extends Component {
     render() {
         return (<div className="App">
             <header>
                 {/* 这种写法应该是经过配置的 */}
                 <img src={logo} alt="" className="App-logo"/>
+                <p> Stage1 start ==================</p>
+                <Stage1/>
             </header>
         </div>);
     }
---
+++ src/components/stage1.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Stage1 extends Component {
+    render() {
+        return (<div className="Stage1">
+
+        </div>);
+    }
+}
+
+export default Stage1;
