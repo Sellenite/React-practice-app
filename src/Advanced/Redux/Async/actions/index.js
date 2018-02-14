16f424f50370622fe31be9af05866af637de74c4
Sellenite <714627528@qq.com>
2018-02-14 11:30:46 +0800

    update

--- src/Advanced/Redux/Async/actions/index.js
+++ src/Advanced/Redux/Async/actions/index.js
@@ -8,24 +8,15 @@ export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 
 /* actions */
 export const selectSubreddit = (subreddit) => {
-    return {
-        type: SELECT_SUBREDDIT,
-        subreddit
-    }
+    return {type: SELECT_SUBREDDIT, subreddit}
 }
 
 export const invalidateSubreddit = (subreddit) => {
-    return {
-        type: INVALIDATE_SUBREDDIT,
-        subreddit
-    }
+    return {type: INVALIDATE_SUBREDDIT, subreddit}
 }
 
 export const requestPosts = (subreddit) => {
-    return {
-        type: REQUEST_POSTS,
-        subreddit
-    }
+    return {type: REQUEST_POSTS, subreddit}
 }
 
 const receivePosts = (subreddit, json) => {
@@ -45,7 +36,7 @@ const fetchPosts = (subreddit) => {
     // 以此来让它自己也能 dispatch action。
     // 这个函数并不需要保持纯净；它还可以带有副作用，包括执行异步 API 请求
     // 但reducer必须保持纯净
-    return (dispatch) => {
+    return(dispatch) => {
         // 首次 dispatch：更新应用的 state 来通知
         // API 请求发起了。
         dispatch(requestPosts(subreddit));
@@ -56,10 +47,10 @@ const fetchPosts = (subreddit) => {
         // 这并不是 redux middleware 所必须的，但这对于我们而言很方便。
         // 这里返回的是fetch的Promise对象，所以fetchPosts可以使用.then()，传参是下面的json，也就是state
         return fetch(`http://www.subreddit.com/r/${subreddit}.json`).then(response => response.json()).then(json =>
-            // 可以多次 dispatch！
-            // 这里，使用 API 请求结果来更新应用的 state。
-            // chunk中最后一个被dispatch的action，是一个普通对象，用来带回同步方式
-            dispatch(receivePosts(subreddit, json)));
+        // 可以多次 dispatch！
+        // 这里，使用 API 请求结果来更新应用的 state。
+        // chunk中最后一个被dispatch的action，是一个普通对象，用来带回同步方式
+        dispatch(receivePosts(subreddit, json)));
         // 在实际应用中，还需要
         // 捕获网络请求的异常。
     }
@@ -84,10 +75,11 @@ export const fetchPostsIfNeeded = (subreddit) => {
 
     // 当缓存的值是可用时，使用缓存值
     // 减少网络请求很有用。
-    return (dispatch, getState) => {
+    return(dispatch, getState) => {
         if (shouldFetchPosts(getState(), subreddit)) {
             // 在 thunk 里 dispatch 另一个 thunk！
             // 这里可以return一个参数，由于fetchPosts返回一个Promise，所以这里的fetchPostsIfNeeded可以使用.then()
+            // redux中的dispatch返回值是返回里面的action
             return dispatch(fetchPosts(subreddit));
         } else {
             // 告诉调用代码不需要再等待，手动触发Promise，让fetchPostsIfNeeded执行后有.then()方法。
